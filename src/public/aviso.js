d38838a9cc323d767ebaec13409ab4f6a3691a1d
Miguel Salazar <yobebe@hotmail.com.ar>
2023-10-02 21:58:31 -0300

    desafio entregable4 terminado

--- src/public/aviso.js
+++ src/public/aviso.js
@@ -1,9 +1,13 @@
 const imagenInput = document.getElementById('imagenInput');
 const aviso = document.getElementById('aviso');
+
 imagenInput.addEventListener('change', function() {
     if (imagenInput.files.length === 0) {
-        alert('Falta la imagen del producto');
-    } else if (imagenInput.files.length <= 1) {
-        alert('Se cargó la imagen');
+        aviso.textContent = 'Falta la imagen del producto';
+        aviso.style.color = 'red';
+    } 
+    else if (imagenInput.files.length <= 1) {
+        aviso.textContent = 'Se cargó la imagen';
+        aviso.style.color = 'green';
     }
 });
